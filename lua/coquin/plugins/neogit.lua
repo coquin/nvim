2820766fb5e599dc83079fa1b587edca809e939f
Denis Kokin <denis.kokin@gmail.com>
2024-05-19 17:32:10 +0200

    Cleaning up neogit config

--- lua/coquin/plugins/neogit.lua
+++ lua/coquin/plugins/neogit.lua
@@ -1,15 +1,3 @@
--- local user_config = vim.g.config.plugins.neogit or {}
---
--- local default_config = {
---   enabled = true,
---   keys = {
---     { "<leader>g<Enter>", "<cmd>Neogit<cr>", desc = "Neogit" },
---   },
---   opts = {},
--- }
---
--- local config = vim.tbl_deep_extend("force", default_config, user_config)
-
 return {
   "NeogitOrg/neogit",
   tag = "v0.0.1",
@@ -19,11 +7,9 @@ return {
     "sindrets/diffview.nvim",        -- optional - Diff integration
     "nvim-telescope/telescope.nvim",
   },
-  -- keys = config.keys,
   keys = {
     { "<leader>g<Enter>", "<cmd>Neogit<cr>", desc = "Neogit" },
   },
-  -- opts = config.opts,
   config = function()
     require("neogit").setup {}
   end,
